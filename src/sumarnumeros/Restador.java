9e737e8833911f6efde3127a37fb32be2fd1b06d
Laura <Lauri161998@gmail.com>
2024-05-10 18:02:19 +0200

    Segundo commit

---
+++ src/sumarnumeros/Restador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sumarnumeros;
+
+/**
+ *
+ * @author Laura
+ */
+public class Restador {
+    public static float restar(float a, float b) {
+        return a - b;
+    }
+}
